9c9022ec99e4d7f7a43f1af4a0eca20a8b1dcde1
shaaibu <shaaibusuleiman@gamil.com>
2023-01-04 15:07:12 +0100

    Add dynamic content for featured section

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const navmenu = document.querySelector('.nav-menu');
-const mainContainer = document.querySelector('.enclose');
-
+const mainContainer = document.querySelector('.main-content');
 
 hamburger.addEventListener('click', () => {
-  console.log('hello wolrd');
   hamburger.classList.toggle('active');
   navmenu.classList.toggle('active');
 });
@@ -18,3 +16,64 @@ navLinks.forEach((link) => {
   });
 });
 
+const content = [
+  {
+    id: 'box1',
+    image: 'assets/images/profileimg.jpeg',
+    name: 'Lowie Dan',
+    work: 'Sity Computer Engineer',
+    comment: 'A sound and outstanding proffesor with proven track.',
+  },
+
+  {
+    id: 'box2',
+    image: 'assets/images/profileimg.jpeg',
+    name: 'Lowie Dan',
+    work: 'Sity Computer Engineer',
+    comment: 'A sound and outstanding proffesor with proven track.',
+  },
+
+  {
+    id: 'box3',
+    image: 'assets/images/profileimg.jpeg',
+    name: 'Lowie Dan',
+    work: 'Sity Computer Engineer',
+    comment: 'A sound and outstanding proffesor with proven track.',
+  },
+];
+
+content.forEach((item) => {
+  mainContainer.innerHTML += `
+  <div class="client-room">
+  
+          <div class="client-container">
+            <div class="client-image">
+              <img src="${item.image}" alt="Cleint image" width="90px" height="100px" class="client-picture">
+            </div>
+      
+            <div class="client-info">
+                <h5>${item.name}</h4>
+                  <p class="client-paragraph">${item.work}</p>
+                  <div class="client-line"></div>
+                  <p class="client-text pad-class">${item.comment}
+                  </p>
+            </div>
+            
+          </div>
+    
+          <div class="client-container">
+            <div class="client-image">
+              <img src="${item.image}" alt="Cleint image" width="90px" height="100px" class="client-picture">
+            </div>
+      
+            <div class="client-info">
+              <h5>${item.name}</h5>
+              <p class="client-paragraph">${item.work}</p>
+              <div class="client-line"></div>
+              <p class="client-text pad-class">${item.comment}
+              </p>
+            </div>
+            
+          </div>
+        </div>`;
+});
